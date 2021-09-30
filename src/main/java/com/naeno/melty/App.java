cb21bedfd8f72adb799e02bfee700da386fba631
Arnaud Dides-Rieul <daakhan@gmail.com>
2021-09-30 12:29:40 +0200

    more work

--- src/main/java/com/naeno/melty/App.java
+++ src/main/java/com/naeno/melty/App.java
@@ -5,6 +5,11 @@ import com.naeno.melty.dao.CharacterDAO;
 import com.naeno.melty.dao.CustomColorDAO;
 import com.naeno.melty.models.Character;
 import com.naeno.melty.models.CustomColor;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.DirectoryCodeResolver;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.plugin.rendering.template.JavalinJte;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlite3.SQLitePlugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
@@ -13,27 +18,43 @@ import io.javalin.Javalin;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.List;
-
-import static io.javalin.plugin.rendering.template.TemplateUtil.model;
 
 /**
  * Hello world!
  */
 public class App {
 
+    public static boolean isProd = false;
+
     public static void main(String[] args) {
 
-        Javalin app = Javalin.create().start(7000);
+        if (args.length > 0 && "prod".equals(args[0])){
+            isProd = true;
+        }
+        JavalinJte.configure(createTemplateEngine());
+        Javalin app = Javalin.create(config -> {
+            config.addStaticFiles(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/static";
+                staticFileConfig.directory = "static";
+                staticFileConfig.location = Location.EXTERNAL;
+                staticFileConfig.precompress = isProd;
+            });
+            config.addStaticFiles(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/images";
+                staticFileConfig.directory = "data/images";
+                staticFileConfig.location = Location.EXTERNAL;
+            });
+        }).start(7000);
         try {
-            Files.createDirectory(Paths.get("images"));
-            Files.createDirectory(Paths.get("db"));
+            Files.createDirectory(Paths.get("data/images"));
+            Files.createDirectory(Paths.get("data/db"));
         } catch (IOException e) {
         }
 
-        Jdbi jdbi = Jdbi.create("jdbc:sqlite:db/database.db").installPlugin(new SqlObjectPlugin()).installPlugin(new SQLitePlugin());
+        Jdbi jdbi = Jdbi.create("jdbc:sqlite:data/db/database.db").installPlugin(new SqlObjectPlugin()).installPlugin(new SQLitePlugin());
         initDb(jdbi);
         jdbi.registerRowMapper(new Character.CharacterMapper());
         jdbi.registerRowMapper(new CustomColor.ColorMapper());
@@ -47,6 +68,15 @@ public class App {
         app.get("/colors", controller::getColors);
         app.get("/colors/<id>", controller::getColor);
         app.post("/colors", controller::addColor);
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        if (isProd) {
+            return TemplateEngine.createPrecompiled(Path.of("jte-classes"), ContentType.Html);
+        } else {
+            DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(Path.of("src", "main", "jte"));
+            return TemplateEngine.create(codeResolver, ContentType.Html);
+        }
     }
 
     public static void initDb(Jdbi jdbi){
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ["src/main/jte/**/*.jte"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+  },
+  variants: {
+  },
+  plugins: [],
+}
