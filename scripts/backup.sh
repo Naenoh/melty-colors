7549dbc7ea7ea4fd07e72c93447980b385d31baf
Arnaud Dides-Rieul <daakhan@gmail.com>
2021-10-03 12:26:31 +0200

    another one

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATE=$(date +%d-%m-%Y)
-BACKUP_DIR=backup
-cd /home/daakhan || exit
-tar -zcvpf $BACKUP_DIR/meltycolors-$DATE.tar.gz /opt/projects/melty-colors/data
+BACKUP_DIR=/home/daakhan/backup
+cd /opt/projects/melty-colors/ || exit
+tar -zcvpf $BACKUP_DIR/meltycolors-$DATE.tar.gz data
 find $BACKUP_DIR/* -mtime +7 -exec rm {} \;
