e32add82bb5c5e3e34d67dfc978c30f2145c07ea
Arnaud Dides-Rieul <daakhan@gmail.com>
2021-10-03 12:10:14 +0200

    small change to backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DATE=$(date +%d-%m-%Y)
 BACKUP_DIR=”/home/daakhan/backup”
-tar -zcvpf $BACKUP_DIR/yeahhub-$DATE.tar.gz /opt/projects/melty-colors/data
+tar -zcvpf $BACKUP_DIR/meltycolors-$DATE.tar.gz /opt/projects/melty-colors/data
 find $BACKUP_DIR/* -mtime +7 -exec rm {} \;
